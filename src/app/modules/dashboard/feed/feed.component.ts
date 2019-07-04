d3928def0eaa40c8dcecb1daa772c033726d1105
Vijay Bhaskar Puttamshetty <vj.bhaskar012@gmail.com>
2019-07-04 10:00:55 +0200

    Handel ip

--- src/app/modules/dashboard/feed/feed.component.ts
+++ src/app/modules/dashboard/feed/feed.component.ts
@@ -17,18 +17,37 @@ export class FeedComponent implements OnInit {
 
   ngOnInit() {
     this.userData = this.helperService.getUserObj();
+    console.log("first userdata",this.userData);
     if(this.userData['user_type'] == '1' && this.userData['exam']){
       this.noExam = false;
       let data = {
-        "username":this.userData['username']
+        "username":this.userData['username'],
+        "ip":{}
       };
+      
 
       this.api.getLabById(this.userData['ip']['lab']).subscribe(resp => {
         this.labData = resp['body'];
       })
-      this.api.getLoggedIp(data).subscribe( resp =>{
+      // var findIP = new Promise(r=>{var w=window,a=new (w['RTCPeerConnection']||w['mozRTCPeerConnection']||w['webkitRTCPeerConnection'])({iceServers:[]}),b=()=>{};a.createDataChannel("");a.createOffer(c=>a.setLocalDescription(c,b,b),b);a.onicecandidate=c=>{try{c.candidate.candidate.match(/([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g).forEach(r)}catch(e){}}})
+      //   findIP.then(function(ip) {
+      //   console.log(ip);
+      //   let data = {
+      //     "ip":{}
+      //   };
+      //   data['ip'] = ip;
+      //   this.api.getLoggedIp(data).subscribe( resp =>{
+      //     this.loggedInIp = resp['body']['data'];
+      //   })
+      // })
+
+      this.helperService.getLoggedInIP().subscribe(ip => {
+        console.log("IP in feed",ip);
+        data['ip'] = ip;
+        this.api.getLoggedIp(data).subscribe( resp =>{
           this.loggedInIp = resp['body']['data'];
-      })
+        })
+      }) 
     }
   }
 
